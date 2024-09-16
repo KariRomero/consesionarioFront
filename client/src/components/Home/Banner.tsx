3451171bae00eaaa493956774b9e0f086775a92c
KariRomero <karinaromero.1993@gmail.com>
2024-09-16 10:44:15 -0300

    se agregó Banner en Home

--- client/src/components/Home/Banner.tsx
+++ client/src/components/Home/Banner.tsx
@@ -1,9 +1,41 @@
-const Banner = () => {
-  return (
-    <div>
-        <h1>BANNER</h1>      
-    </div>
-  )
-}
+'use client'
+import { useState, useEffect } from 'react';
 
-export default Banner
+const Banner = () => {  
+
+  const slides= [ '/car1.jpg', '/car2.jpg', '/car3.jpg'];
+
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    const newIndex = (currentIndex + 1) % slides.length;
+    setCurrentIndex(newIndex);
+  };
+
+  
+  useEffect(() => {
+    if (slides.length > 0) {
+      const interval = setInterval(nextSlide, 5000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [currentIndex, slides]);
+
+  if (slides.length === 0) {
+    
+    return <div>No hay banners disponibles.</div>;
+  }
+
+  return (
+    <div className='max-w h-[720px] w-full relative group bg-black block'>
+
+      <div style={{ backgroundImage: `url(${slides[currentIndex]})` }} className='w-full h-full duration-500 bg-center bg-cover'></div>
+      
+    </div>
+  );
+};
+
+export default Banner;
