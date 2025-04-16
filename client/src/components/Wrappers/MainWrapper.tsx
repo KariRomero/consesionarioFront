2ba0236196cbde25099c8e344ca741c79971f203
KariRomero <karinaromero.1993@gmail.com>
2025-04-16 20:55:02 -0300

    renderiza Loader si loading de cars true

--- client/src/components/Wrappers/MainWrapper.tsx
+++ client/src/components/Wrappers/MainWrapper.tsx
@@ -1,16 +1,26 @@
-'use client'
-import { usePathname } from 'next/navigation';
+'use client';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 import NavBar from '@/components/NavBar/NavBar';
+import Loader from '../Loader/Loader';
 
 export default function MainWrapper({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
 
-  const showNavBar = !pathname.startsWith('/admin');
+  const { loading } = useSelector((state: RootState) => state.cars);
 
   return (
-    <>
-      {showNavBar && <NavBar />}
-      {children}
-    </>
+    <div className="w-full min-h-screen flex flex-col">
+      {/* Muestra el Loader solo cuando loading de cars sea true */}
+      {loading ? (
+        <Loader />
+      ) : (
+        <NavBar />
+      )}
+
+      {/* Contenido principal */}
+      <div className="w-full px-4 sm:px-6 md:px-8 bg-white mt-28">
+        {children}
+      </div>
+    </div>
   );
 }
