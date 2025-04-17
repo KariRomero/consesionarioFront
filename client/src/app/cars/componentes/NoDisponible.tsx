89f1763a32a431e0e2099a63e26f88bf8f1d8c2f
KariRomero <karinaromero.1993@gmail.com>
2025-04-16 21:00:29 -0300

    modifique h

--- client/src/app/cars/componentes/NoDisponible.tsx
+++ client/src/app/cars/componentes/NoDisponible.tsx
@@ -6,7 +6,7 @@ type NoDisponibleProps={
 
 export default function NoDisponible({ brandOTipo } : NoDisponibleProps ) {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
+    <div className='w-full h-screen flex justify-center items-start'>
         <h1>Lo sentimos, en este momento no tenemos disponibilidad de vehiculos {brandOTipo}</h1>      
     </div>
   )
