9f0c9f1498653322ea60eb19d66e7db111b9fd9b
KariRomero <karinaromero.1993@gmail.com>
2025-05-10 16:02:02 -0300

    refactor: implementacion del nuevo componente CategoriasSection

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,21 +1,35 @@
 'use client'
 import Banner from './Banner';
-import Tipos from './Tipos/Tipos';
 import Description from './Description';
-// import MostSearchedCars from './MostSearchedCars';
-import Brands from './Brands/Brands';
 import Contact from './Contact';
 import Footer from '../Footer/Footer';
+import CategoriasSection from './CategoriasSection';
+import { fetchTipos } from '@/redux/slices/tiposSlice';
+import { RootState } from '@/redux/store';
+import { fetchBrands } from '@/redux/slices/brandsSlice';
 
 const Home: React.FC = () => {
  
   return (
     <section className='flex flex-col items-center space-y-28'>
       <Banner/>
-      <Tipos/>
+      <CategoriasSection
+      elementType='tipo'
+      title='Tenemos una gran variedad para vos'
+      subtitle='Encontrá tu proximo vehículo'
+      selector={(state: RootState) => state.tipos.tipos}
+      fetchFunction={fetchTipos}
+      linkHref='/cars'      
+      />
       <Description/>
-      {/* <MostSearchedCars/> */}
-      <Brands/>
+      <CategoriasSection
+      elementType='brand'
+      title='Encontrá el auto que mejor se adapte a vos'
+      subtitle='Las mejores marcas'
+      selector={(state: RootState) => state.brands.brands}
+      fetchFunction={fetchBrands}
+      linkHref='/cars'
+      />
       <Contact/>
       <Footer/>
     </section>
